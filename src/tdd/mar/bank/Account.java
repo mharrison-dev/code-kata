cb96ff5f5054b1b295ecfffe8c15614b845ef447
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-03-10 05:18:45 -0600

    Bank: Print statement after making a deposit

--- src/tdd/mar/bank/Account.java
+++ src/tdd/mar/bank/Account.java
@@ -1,9 +1,20 @@
 package tdd.mar.bank;
 
+import java.time.LocalDate;
+
 public final class Account implements AccountService {
+    private int amount = 0;
+    private int balance = 0;
+    private final LocalDate accountCreationDate;
+
+    public Account(LocalDate accountCreationDate) {
+        this.accountCreationDate = accountCreationDate;
+    }
+
     @Override
     public void deposit(int amount) {
-
+        this.amount = amount;
+        this.balance = amount;
     }
 
     @Override
@@ -13,6 +24,32 @@ public final class Account implements AccountService {
 
     @Override
     public void printStatement() {
-        System.out.print("Date||Amount||Balance");
+        String header = "Date||Amount||Balance";
+        System.out.print(header);
+
+        if (amount > 0) {
+            String entry = getEntry();
+            System.out.print("\n" + entry);
+        }
+    }
+
+    private String getEntry() {
+        String[] entryComponents = new String[3];
+        entryComponents[0] = formatDate(accountCreationDate.toString());
+        entryComponents[1] = String.valueOf(amount);
+        entryComponents[2] = String.valueOf(balance);
+
+        return String.join("||", entryComponents);
+    }
+
+    private String formatDate(String date) {
+        // Swap the position of the day and year
+        String[] dateComponents = date.split("-");
+        String year = dateComponents[0];
+        dateComponents[0] = dateComponents[2];
+        dateComponents[2] = year;
+
+        // Replace each "-" with a "/"
+        return String.join("/", dateComponents);
     }
 }
--- test/tdd/mar/bank/AccountTest.java
+++ test/tdd/mar/bank/AccountTest.java
@@ -2,10 +2,9 @@ package tdd.mar.bank;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,10 +16,30 @@ class AccountTest {
         PrintStream testOut = new PrintStream(byteArrayOutputStreamOfTestOut);
         System.setOut(testOut);
 
-        AccountService accountService = new Account();
+        LocalDate accountCreationDate = LocalDate.of(2012, 1, 10);
+        AccountService accountService = new Account(accountCreationDate);
         accountService.printStatement();
 
         String expectedStatement = "Date||Amount||Balance";
+        String actualStatement = byteArrayOutputStreamOfTestOut.toString();
+        assertEquals(expectedStatement, actualStatement);
+
+        System.setOut(productionOut);
+    }
+
+    @Test
+    public void printStatementAfterMakingDeposit() {
+        PrintStream productionOut = System.out;
+        ByteArrayOutputStream byteArrayOutputStreamOfTestOut = new ByteArrayOutputStream();
+        PrintStream testOut = new PrintStream(byteArrayOutputStreamOfTestOut);
+        System.setOut(testOut);
+
+        LocalDate accountCreationDate = LocalDate.of(2012, 1, 10);
+        AccountService accountService = new Account(accountCreationDate);
+        accountService.deposit(1000);
+        accountService.printStatement();
+
+        String expectedStatement = "Date||Amount||Balance\n10/01/2012||1000||1000";
         String actualStatement = byteArrayOutputStreamOfTestOut.toString();
         assertEquals(expectedStatement, actualStatement);
 
