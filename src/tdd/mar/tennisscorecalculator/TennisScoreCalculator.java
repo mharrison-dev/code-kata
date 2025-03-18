944de330ce8b0e304a06dee59bfeedcf27ecd6b6
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-03-18 04:23:08 -0600

    Tennis Score Calculator: Return respective score for each point below 4 for player two

--- src/tdd/mar/tennisscorecalculator/TennisScoreCalculator.java
+++ src/tdd/mar/tennisscorecalculator/TennisScoreCalculator.java
@@ -1,13 +1,13 @@
 package tdd.mar.tennisscorecalculator;
 
 public class TennisScoreCalculator {
-    private String[] terminologyForEachPointBelowFour = {"Love", "Fifteen", "Thirty", "Forty"};
+    private final String[] terminologyForEachPointBelowFour = {"Love", "Fifteen", "Thirty", "Forty"};
 
     public String score(int playerOnePoints, int playerTwoPoints) {
         if (playerOnePoints == 0 && playerTwoPoints == 0) {
             return "Love All";
         }
 
-        return terminologyForEachPointBelowFour[playerOnePoints] + "-Fifteen";
+        return terminologyForEachPointBelowFour[playerOnePoints] + "-" + terminologyForEachPointBelowFour[playerTwoPoints];
     }
 }
--- test/tdd/mar/tennisscorecalculator/TennisScoreCalculatorTest.java
+++ test/tdd/mar/tennisscorecalculator/TennisScoreCalculatorTest.java
@@ -1,5 +1,6 @@
 package tdd.mar.tennisscorecalculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -7,11 +8,17 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TennisScoreCalculatorTest {
+    private TennisScoreCalculator tennisScoreCalculator = null;
+
+    @BeforeEach
+    public void setUp() {
+        tennisScoreCalculator = new TennisScoreCalculator();
+    }
+
     @Test
     public void shouldReturnLoveAll_whenBothPlayersHaveScoredZeroPoints() {
         int playerOnePoints = 0;
         int playerTwoPoints = 0;
-        TennisScoreCalculator tennisScoreCalculator = new TennisScoreCalculator();
 
         String expectedScore = "Love All";
         String actualScore = tennisScoreCalculator.score(playerOnePoints, playerTwoPoints);
@@ -23,9 +30,19 @@ class TennisScoreCalculatorTest {
     public void shouldReturnRespectiveScoreForEachPointBelowFour_forPlayerOne(String example) {
         int playerOnePoints = Integer.parseInt(example.split(",")[0]);
         int playerTwoPoints = 1;
-        TennisScoreCalculator tennisScoreCalculator = new TennisScoreCalculator();
 
         String expectedScore = example.split(",")[1] + "-Fifteen";
+        String actualScore = tennisScoreCalculator.score(playerOnePoints, playerTwoPoints);
+        assertEquals(expectedScore, actualScore);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0,Love", "1,Fifteen", "2,Thirty", "3,Forty"})
+    public void shouldReturnRespectiveScoreForEachPointBelowFour_forPlayerTwo(String example) {
+        int playerOnePoints = 1;
+        int playerTwoPoints = Integer.parseInt(example.split(",")[0]);
+
+        String expectedScore = "Fifteen-" + example.split(",")[1];
         String actualScore = tennisScoreCalculator.score(playerOnePoints, playerTwoPoints);
         assertEquals(expectedScore, actualScore);
     }
