d4e726a4462326c7d829d6acb228d1a9ed7da475
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-03-18 04:20:02 -0600

    Tennis Score Calculator: Return respective score for each point below 4 for player one

--- src/tdd/mar/tennisscorecalculator/TennisScoreCalculator.java
+++ src/tdd/mar/tennisscorecalculator/TennisScoreCalculator.java
@@ -1,7 +1,13 @@
 package tdd.mar.tennisscorecalculator;
 
 public class TennisScoreCalculator {
-    public String score(int playerOnePoints, int playerTwoPoints){
-        return "Love All";
+    private String[] terminologyForEachPointBelowFour = {"Love", "Fifteen", "Thirty", "Forty"};
+
+    public String score(int playerOnePoints, int playerTwoPoints) {
+        if (playerOnePoints == 0 && playerTwoPoints == 0) {
+            return "Love All";
+        }
+
+        return terminologyForEachPointBelowFour[playerOnePoints] + "-Fifteen";
     }
 }
--- test/tdd/mar/tennisscorecalculator/TennisScoreCalculatorTest.java
+++ test/tdd/mar/tennisscorecalculator/TennisScoreCalculatorTest.java
@@ -1,6 +1,8 @@
 package tdd.mar.tennisscorecalculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,6 +14,18 @@ class TennisScoreCalculatorTest {
         TennisScoreCalculator tennisScoreCalculator = new TennisScoreCalculator();
 
         String expectedScore = "Love All";
+        String actualScore = tennisScoreCalculator.score(playerOnePoints, playerTwoPoints);
+        assertEquals(expectedScore, actualScore);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0,Love", "1,Fifteen", "2,Thirty", "3,Forty"})
+    public void shouldReturnRespectiveScoreForEachPointBelowFour_forPlayerOne(String example) {
+        int playerOnePoints = Integer.parseInt(example.split(",")[0]);
+        int playerTwoPoints = 1;
+        TennisScoreCalculator tennisScoreCalculator = new TennisScoreCalculator();
+
+        String expectedScore = example.split(",")[1] + "-Fifteen";
         String actualScore = tennisScoreCalculator.score(playerOnePoints, playerTwoPoints);
         assertEquals(expectedScore, actualScore);
     }
