1f17643555b4be2401b3a74fd64c0a6cfaba4a95
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-03-20 05:32:04 -0600

    Leap Year: Should return true when the year is divisible by 4

--- src/tdd/mar/leapyear/LeapYear.java
+++ src/tdd/mar/leapyear/LeapYear.java
@@ -2,6 +2,10 @@ package tdd.mar.leapyear;
 
 public class LeapYear {
     public boolean validate(int year) {
-        return year % 400 == 0;
+        if (year % 100 == 0 && year % 400 != 0) {
+            return false;
+        }
+
+        return year % 4 == 0;
     }
 }
--- test/tdd/mar/leapyear/LeapYearTest.java
+++ test/tdd/mar/leapyear/LeapYearTest.java
@@ -1,5 +1,6 @@
 package tdd.mar.leapyear;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -7,18 +8,26 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LeapYearTest {
+    private LeapYear leapYear = null;
+
+    @BeforeEach
+    public void setUp() {
+        leapYear = new LeapYear();
+    }
+
     @ParameterizedTest
     @ValueSource(ints = {1997, 2001, 2015})
     public void shouldReturnFalse_whenYearIsNotDivisibleByFour(int year) {
-        LeapYear leapYear = new LeapYear();
-
         assertFalse(leapYear.validate(year));
     }
 
     @Test
     public void shouldReturnFalse_whenYearIsDivisibleByOneHundredButNotByFourHundred() {
-        LeapYear leapYear = new LeapYear();
-
         assertFalse(leapYear.validate(1800));
+    }
+
+    @Test
+    public void shouldReturnTrue_whenYearIsDivisibleByFour() {
+        assertTrue(leapYear.validate(1996));
     }
 }
