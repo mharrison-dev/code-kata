de53fc59fc45c0f04b19a728311ef9879911ec90
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-03-23 16:01:45 -0600

    N-Queens Puzzle: The fringe generation logic should return four boards that each has a queen in different positions when it is given an empty two by two board

---
+++ src/tdd/mar/nqueenspuzzle/FringeGenerationLogic.java
@@ -0,0 +1,24 @@
+package tdd.mar.nqueenspuzzle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FringeGenerationLogic {
+    public List<String> generateFringe(String board) {
+        List<String> fringe = new ArrayList<>();
+        for (int i = 0; i < board.length(); i++) {
+            if (board.charAt(i) == '.') {
+                String newBoard = placeQueen(i, board);
+                fringe.add(newBoard);
+            }
+        }
+
+        return fringe;
+    }
+
+    private static String placeQueen(int i, String board) {
+        StringBuilder boardBuilder = new StringBuilder(board);
+        boardBuilder.setCharAt(i, 'Q');
+        return boardBuilder.toString();
+    }
+}
---
+++ test/tdd/mar/nqueenspuzzle/FringeGenerationLogicTest.java
@@ -0,0 +1,19 @@
+package tdd.mar.nqueenspuzzle;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FringeGenerationLogicTest {
+    @Test
+    public void shouldReturnFourBoardsThatEachContainOneQueen_whenGivenEmptyTwoByTwoBoard() {
+        FringeGenerationLogic fringeGenerationLogic = new FringeGenerationLogic();
+        String emptyBoard = "..\n..";
+
+        List<String> expectedBoards = List.of("Q.\n..", ".Q\n..", "..\nQ.", "..\n.Q");
+        List<String> actualBoards = fringeGenerationLogic.generateFringe(emptyBoard);
+        assertTrue(expectedBoards.containsAll(actualBoards));
+    }
+}
