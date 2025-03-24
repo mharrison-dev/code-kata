72838aff39e3af36140195c7df8e66adadad909e
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-03-24 06:56:57 -0600

    N-Queens Puzzle: Queen placement logic should return a board with a queen that is surrounded by 'X's when the queen is placed in the center of an empty three by three board

--- test/tdd/mar/nqueenspuzzle/QueenPlacementLogic.java
+++ test/tdd/mar/nqueenspuzzle/QueenPlacementLogic.java
@@ -1,7 +1,47 @@
 package tdd.mar.nqueenspuzzle;
 
 public class QueenPlacementLogic {
-    public String placeQueen(int rowIndex, int columnIndex) {
-        return "Q";
+    public String placeQueen(int rowIndex, int columnIndex, String board) {
+        String[][] boardArray = convertToArray(board);
+        for (int i = rowIndex - 1; i <= rowIndex + 1; i++) {
+            if (i > -1 && i < boardArray.length) {
+                for (int j = columnIndex - 1; j <= columnIndex + 1; j++) {
+                    if (j > -1 && j < boardArray.length) {
+                        boardArray[i][j] = "X";
+                    }
+                }
+            }
+        }
+
+        boardArray[rowIndex][columnIndex] = "Q";
+
+        return convertToString(boardArray);
+    }
+
+    private String[][] convertToArray(String board) {
+        String[] ranks = board.split("\n");
+        String[][] boardArray = new String[ranks.length][ranks.length];
+        for (int i = 0; i < ranks.length; i++) {
+            String currentRank = ranks[i];
+            String[] currentFiles = currentRank.split("");
+            System.arraycopy(currentFiles, 0, boardArray[i], 0, currentFiles.length);
+        }
+
+        return boardArray;
+    }
+
+    private String convertToString(String[][] boardArray) {
+        String board = "";
+        for (int i = 0; i < boardArray.length; i++) {
+            if (i > 0) {
+                board += "\n";
+            }
+
+            for (int j = 0; j < boardArray.length; j++) {
+                board += boardArray[i][j];
+            }
+        }
+
+        return board;
     }
 }
--- test/tdd/mar/nqueenspuzzle/QueenPlacementLogicTest.java
+++ test/tdd/mar/nqueenspuzzle/QueenPlacementLogicTest.java
@@ -11,7 +11,17 @@ class QueenPlacementLogicTest {
         QueenPlacementLogic queenPlacementLogic = new QueenPlacementLogic();
 
         String expectedBoard = "Q";
-        String actualBoard = queenPlacementLogic.placeQueen(0, 0);
+        String actualBoard = queenPlacementLogic.placeQueen(0, 0, board);
+        assertEquals(expectedBoard, actualBoard);
+    }
+
+    @Test
+    public void shouldReturnBoardWithQueenSurroundedByXs_whenQueenIsPlacedInCenterOfEmptyThreeByThreeBoard() {
+        String board = "...\n...\n...";
+        QueenPlacementLogic queenPlacementLogic = new QueenPlacementLogic();
+
+        String expectedBoard = "XXX\nXQX\nXXX";
+        String actualBoard = queenPlacementLogic.placeQueen(1, 1, board);
         assertEquals(expectedBoard, actualBoard);
     }
 }
