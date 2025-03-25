2a9b880a21c28e23f589f46fbd6f619dd27b3dd1
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-03-25 03:53:58 -0600

    N-Queens Puzzle: Should return one solution for a one by one board

---
+++ src/tdd/mar/nqueenspuzzle/NQueensPuzzle.java
@@ -0,0 +1,9 @@
+package tdd.mar.nqueenspuzzle;
+
+import java.util.List;
+
+public class NQueensPuzzle {
+    public List<String> solve(String board) {
+        return List.of("Q");
+    }
+}
---
+++ test/tdd/mar/nqueenspuzzle/NQueensPuzzleTest.java
@@ -0,0 +1,19 @@
+package tdd.mar.nqueenspuzzle;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NQueensPuzzleTest {
+    @Test
+    public void shouldReturnOneBoard_whenGivenEmptyOneByOneBoard() {
+        String board = ".";
+        NQueensPuzzle nQueensPuzzle = new NQueensPuzzle();
+
+        List<String> expectedSolutionList = List.of("Q");
+        List<String> actualSolutionList = nQueensPuzzle.solve(board);
+        assertTrue(expectedSolutionList.containsAll(actualSolutionList));
+    }
+}
