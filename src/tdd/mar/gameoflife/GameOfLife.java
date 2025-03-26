3742506cbb668e9f3880d3ff8715b2a98669b30b
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-03-26 05:06:19 -0600

    Game of Life: Should not kill any cell that has two neighbours on its opposing sides

--- src/tdd/mar/gameoflife/GameOfLife.java
+++ src/tdd/mar/gameoflife/GameOfLife.java
@@ -2,6 +2,27 @@ package tdd.mar.gameoflife;
 
 public class GameOfLife {
     public int[][] update(int[][] board) {
+        if (foundTwoNeighbours(board)) {
+            return new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
+        }
+
         return new int[][]{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+    }
+
+    private boolean foundTwoNeighbours(int[][] board) {
+        int rowIndex = 1;
+        int columnIndex = 1;
+
+        for (int i = -1; i <= 0; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i != 0 || j != 0) {
+                    if (board[rowIndex + i][columnIndex + j] == 1 && board[rowIndex - i][columnIndex - j] == 1) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 }
--- test/tdd/mar/gameoflife/GameOfLifeTest.java
+++ test/tdd/mar/gameoflife/GameOfLifeTest.java
@@ -1,5 +1,6 @@
 package tdd.mar.gameoflife;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -21,6 +22,16 @@ class GameOfLifeTest {
         assertTrue(Arrays.deepEquals(expectedBoard, actualBoard), getErrorMessage(expectedBoard, actualBoard));
     }
 
+    @ParameterizedTest
+    @MethodSource("getAllThreeByThreeBoardsThatHaveOneCellWithTwoNeighboursOnOpposingSides")
+    public void shouldNotKillCell_whenCellHasTwoNeighboursOnOpposingSides(int[][] board) {
+        GameOfLife gameOfLife = new GameOfLife();
+
+        int[][] expectedBoard = {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
+        int[][] actualBoard = gameOfLife.update(board);
+        assertTrue(Arrays.deepEquals(expectedBoard, actualBoard), getErrorMessage(expectedBoard, actualBoard));
+    }
+
     private static Stream<Arguments> getAllThreeByThreeBoardsThatHaveOneCellWithFewerThanTwoNeighbours() {
         return Stream.of(
                 arguments((Object) new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}),
@@ -32,6 +43,15 @@ class GameOfLifeTest {
                 arguments((Object) new int[][]{{0, 0, 0}, {0, 1, 0}, {1, 0, 0}}),
                 arguments((Object) new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 1, 0}}),
                 arguments((Object) new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 1}})
+        );
+    }
+
+    private static Stream<Arguments> getAllThreeByThreeBoardsThatHaveOneCellWithTwoNeighboursOnOpposingSides() {
+        return Stream.of(
+                arguments((Object) new int[][]{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}),
+                arguments((Object) new int[][]{{0, 1, 0}, {0, 1, 0}, {0, 1, 0}}),
+                arguments((Object) new int[][]{{0, 0, 1}, {0, 1, 0}, {1, 0, 0}}),
+                arguments((Object) new int[][]{{0, 0, 0}, {1, 1, 1}, {0, 0, 0}})
         );
     }
 
