5b826837ff91988734af713ca6aef4828e178d2c
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-03-28 03:39:05 -0600

    Look-and-say Sequence: Should return a string that contains one followed by the given member if the given member only has one digit

--- src/tdd/mar/lookandsaysequence/LookAndSaySequence.java
+++ src/tdd/mar/lookandsaysequence/LookAndSaySequence.java
@@ -2,6 +2,8 @@ package tdd.mar.lookandsaysequence;
 
 public class LookAndSaySequence {
     public String nextMember(String member) {
-        return "";
+        return (member.isEmpty())
+                ? ""
+                : "1" + member;
     }
 }
--- test/tdd/mar/lookandsaysequence/LookAndSaySequenceTest.java
+++ test/tdd/mar/lookandsaysequence/LookAndSaySequenceTest.java
@@ -1,6 +1,8 @@
 package tdd.mar.lookandsaysequence;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,6 +14,16 @@ class LookAndSaySequenceTest {
 
         String expectedNextMember = "";
         String actualNextMember = lookAndSaySequence.nextMember(emptyString);
+        assertEquals(expectedNextMember, actualNextMember);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "5"})
+    public void shouldReturnOneFollowedByGivenMember_whenGivenMemberContainsOneDigit(String member) {
+        LookAndSaySequence lookAndSaySequence = new LookAndSaySequence();
+
+        String expectedNextMember = "1" + member;
+        String actualNextMember = lookAndSaySequence.nextMember(member);
         assertEquals(expectedNextMember, actualNextMember);
     }
 }
