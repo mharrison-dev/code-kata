8fd284e3f3459b43854c1474b066f22874dcb4af
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-03-30 14:21:56 -0600

    Odd Even String Sort: Should return a string that has the even characters separated from the odd characters of the given string

--- src/tdd/mar/oddevenstringsort/OddEvenStringSort.java
+++ src/tdd/mar/oddevenstringsort/OddEvenStringSort.java
@@ -1,7 +1,41 @@
 package tdd.mar.oddevenstringsort;
 
+import java.util.stream.IntStream;
+
 public class OddEvenStringSort {
     public String sort(String string) {
-        return String.join(" ", string.split(""));
+        String evenString = getEvenString(string);
+        if (evenString.isEmpty()) {
+            return "";
+        }
+
+        String oddString = getOddString(string);
+        if (oddString.isEmpty()) {
+            return evenString;
+        }
+
+        return evenString + " " + oddString;
+    }
+
+    private String getEvenString(String string) {
+        return IntStream.range(0, string.length())
+                .filter(i -> i % 2 == 0)
+                .collect(
+                        StringBuilder::new,
+                        (builder, i) -> builder.append(string.charAt(i)),
+                        StringBuilder::append
+                )
+                .toString();
+    }
+
+    private String getOddString(String string) {
+        return IntStream.range(0, string.length())
+                .filter(i -> i % 2 != 0)
+                .collect(
+                        StringBuilder::new,
+                        (builder, i) -> builder.append(string.charAt(i)),
+                        StringBuilder::append
+                )
+                .toString();
     }
 }
--- test/tdd/mar/oddevenstringsort/OddEvenStringSortTest.java
+++ test/tdd/mar/oddevenstringsort/OddEvenStringSortTest.java
@@ -1,14 +1,21 @@
 package tdd.mar.oddevenstringsort;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OddEvenStringSortTest {
+    private OddEvenStringSort oddEvenStringSort = null;
+
+    @BeforeEach
+    public void setUp() {
+        oddEvenStringSort = new OddEvenStringSort();
+    }
+
     @Test
     public void shouldReturnEmptyString_whenGivenEmptyString() {
         String emptyString = "";
-        OddEvenStringSort oddEvenStringSort = new OddEvenStringSort();
 
         String expectedString = "";
         String actualString = oddEvenStringSort.sort(emptyString);
@@ -18,7 +25,6 @@ class OddEvenStringSortTest {
     @Test
     public void shouldReturnStringOfLengthOne_whenGivenStringOfLengthOne() {
         String string = "a";
-        OddEvenStringSort oddEvenStringSort = new OddEvenStringSort();
 
         String expectedString = "a";
         String actualString = oddEvenStringSort.sort(string);
@@ -28,9 +34,17 @@ class OddEvenStringSortTest {
     @Test
     public void shouldReturnStringWithSpaceSplittingTwoCharacters_whenGivenStringOfLengthTwo() {
         String string = "ab";
-        OddEvenStringSort oddEvenStringSort = new OddEvenStringSort();
 
         String expectedString = "a b";
+        String actualString = oddEvenStringSort.sort(string);
+        assertEquals(expectedString, actualString);
+    }
+
+    @Test
+    public void shouldReturnStringWithEvenCharactersSeparatedFromOddCharacters_whenGivenStringThatIsLongerThanTwoCharacters() {
+        String string = "abc";
+
+        String expectedString = "ac b";
         String actualString = oddEvenStringSort.sort(string);
         assertEquals(expectedString, actualString);
     }
