9fba40e6e01ea54cd2c2b3f9bed5be08c9c625c4
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-03-31 04:28:26 -0600

    Prime Factorization: should return an empty list for any integer less than two

---
+++ src/tdd/primefactorization/PrimeFactorization.java
@@ -0,0 +1,10 @@
+package tdd.primefactorization;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactorization {
+    public List<Integer> of(int number) {
+        return new ArrayList<>();
+    }
+}
---
+++ test/tdd/primefactorization/PrimeFactorizationTest.java
@@ -0,0 +1,19 @@
+package tdd.primefactorization;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimeFactorizationTest {
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 1})
+    public void shouldReturnEmptyList_whenGivenNumberLessThanTwo(int number) {
+        PrimeFactorization primeFactorization = new PrimeFactorization();
+
+        List<Integer> primeFactors = primeFactorization.of(number);
+        assertTrue(primeFactors.isEmpty());
+    }
+}
