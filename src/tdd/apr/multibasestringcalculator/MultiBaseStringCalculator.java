75f84e0d5b41a21554e12c6cec6ed659a5db7191
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-04-03 04:01:06 -0600

    Multi-base String Calculator: should return "10" when adding "1" and "1" in base two

--- src/tdd/apr/multibasestringcalculator/MultiBaseStringCalculator.java
+++ src/tdd/apr/multibasestringcalculator/MultiBaseStringCalculator.java
@@ -2,6 +2,10 @@ package tdd.apr.multibasestringcalculator;
 
 public class MultiBaseStringCalculator {
     public String add(int base, String addend, String otherAddend) {
+        int sumInBaseTen = Integer.parseInt(addend) + Integer.parseInt(otherAddend);
+        if (sumInBaseTen == base) {
+            return "10";
+        }
         return String.valueOf(Integer.parseInt(addend) + Integer.parseInt(otherAddend));
     }
 }
--- test/tdd/apr/multibasestringcalculator/MultiBaseStringCalculatorTest.java
+++ test/tdd/apr/multibasestringcalculator/MultiBaseStringCalculatorTest.java
@@ -1,13 +1,20 @@
 package tdd.apr.multibasestringcalculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MultiBaseStringCalculatorTest {
+    private MultiBaseStringCalculator multiBaseStringCalculator = null;
+
+    @BeforeEach
+    public void setUp() {
+        multiBaseStringCalculator = new MultiBaseStringCalculator();
+    }
+
     @Test
     public void shouldReturnTwo_forOnePlusOne_inBaseHigherThanTwo() {
-        MultiBaseStringCalculator multiBaseStringCalculator = new MultiBaseStringCalculator();
         int base = 3;
         String addend = "1";
         String otherAddend = "1";
@@ -19,12 +26,22 @@ public class MultiBaseStringCalculatorTest {
 
     @Test
     public void shouldReturnThree_forOnePlusTwo_inBaseHigherThanThree() {
-        MultiBaseStringCalculator multiBaseStringCalculator = new MultiBaseStringCalculator();
         int base = 4;
         String addend = "1";
         String otherAddend = "2";
 
         String expectedSum = "3";
+        String actualSum = multiBaseStringCalculator.add(base, addend, otherAddend);
+        assertEquals(expectedSum, actualSum);
+    }
+
+    @Test
+    public void shouldReturnOneZero_forOnePlusOne_inBaseTwo() {
+        int base = 2;
+        String addend = "1";
+        String otherAddend = "1";
+
+        String expectedSum = "10";
         String actualSum = multiBaseStringCalculator.add(base, addend, otherAddend);
         assertEquals(expectedSum, actualSum);
     }
