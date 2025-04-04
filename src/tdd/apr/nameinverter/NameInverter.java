781755b64e94026521e9f87a434ecf203c77a16d
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-04-04 04:58:41 -0600

    Name Inverter: refactor code

--- src/tdd/apr/nameinverter/NameInverter.java
+++ src/tdd/apr/nameinverter/NameInverter.java
@@ -1,7 +1,18 @@
 package tdd.apr.nameinverter;
 
+import java.util.stream.IntStream;
+
 public class NameInverter {
-    private static final String HONORIFICS_REGEX = "^(Mr|Mrs|Miss|Mx|Ind|Misc)[.]*\\s";
+    private static final int FORENAME = 0;
+    private static final int SURNAME = 1;
+    private static final int FIRST_POSTNOMINAL = 2;
+
+    private static final String SPACE = " ";
+    private static final String EMPTY_SPACE = "";
+    private static final String NAME_DELIMITER = ", ";
+
+    private static final String EXCESSIVE_WHITESPACE = "\\s+";
+    private static final String HONORIFICS = "^(Mr|Mrs|Miss|Mx|Ind|Misc)[.]*\\s";
 
     public String invert(String name) {
         String[] nameComponents = getNameComponents(name);
@@ -11,28 +22,26 @@ public class NameInverter {
     private static String[] getNameComponents(String name) {
         return name
                 .trim()
-                .replaceAll("\\s+", " ")
-                .replaceAll(HONORIFICS_REGEX, "")
-                .split(" ");
+                .replaceAll(EXCESSIVE_WHITESPACE, SPACE)
+                .replaceAll(HONORIFICS, EMPTY_SPACE)
+                .split(SPACE);
     }
 
     private static String buildInvertedName(String[] nameComponents) {
-        StringBuilder invertedNameBuilder = new StringBuilder();
-
         if (nameComponents.length == 1) {
-            invertedNameBuilder.append(nameComponents[0]);
-        } else {
-            invertedNameBuilder
-                    .append(nameComponents[1])
-                    .append(", ")
-                    .append(nameComponents[0]);
-
-            for (int i = 2; i < nameComponents.length; i++) {
-                invertedNameBuilder
-                        .append(" ")
-                        .append(nameComponents[i]);
-            }
+            return nameComponents[0];
         }
+
+        StringBuilder invertedNameBuilder = new StringBuilder();
+        invertedNameBuilder
+                .append(nameComponents[SURNAME])
+                .append(NAME_DELIMITER)
+                .append(nameComponents[FORENAME]);
+
+        IntStream.range(FIRST_POSTNOMINAL, nameComponents.length)
+                .forEach(postnominal -> invertedNameBuilder
+                        .append(SPACE)
+                        .append(nameComponents[postnominal]));
 
         return invertedNameBuilder.toString();
     }
