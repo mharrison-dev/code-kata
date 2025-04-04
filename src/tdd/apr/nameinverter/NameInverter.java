55d0607b51eb39d680e9480df462cd3706c85d03
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-04-04 03:46:07 -0600

    Name Inverter: should only return a forename or surname when only given a forename or surname

---
+++ src/tdd/apr/nameinverter/NameInverter.java
@@ -0,0 +1,7 @@
+package tdd.apr.nameinverter;
+
+public class NameInverter {
+    public String invert(String name) {
+        return name;
+    }
+}
---
+++ test/tdd/apr/nameinverter/NameInverterTest.java
@@ -0,0 +1,17 @@
+package tdd.apr.nameinverter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NameInverterTest {
+    @Test
+    public void shouldReturnForenameOrSurname_whenOnlyGivenForenameOrSurname() {
+        String surname = "Smith";
+        NameInverter nameInverter = new NameInverter();
+
+        String expectedInversion = "Smith";
+        String actualInversion = nameInverter.invert(surname);
+        assertEquals(expectedInversion, actualInversion);
+    }
+}
