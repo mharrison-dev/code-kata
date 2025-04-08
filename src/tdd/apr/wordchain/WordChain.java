1cb2a4e53d9678555f96fedc4cbd0fa57ad18883
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-04-08 04:32:03 -0600

    Word Chain: should return empty string if last word is not in the dictionary

--- src/tdd/apr/wordchain/WordChain.java
+++ src/tdd/apr/wordchain/WordChain.java
@@ -4,12 +4,26 @@ public class WordChain {
     private String[] dictionary;
 
     public String getChain(String firstWord, String lastWord) {
+        if (!foundInDictionary(lastWord)) {
+            return "";
+        }
+
         String middleWord = getMiddleWord(firstWord);
         if (middleWord == null) {
             return "";
         } else {
             return firstWord + " > " + middleWord + " > " + lastWord;
         }
+    }
+
+    private boolean foundInDictionary(String targetWord) {
+        for (String word : dictionary) {
+            if (word.equals(targetWord)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private String getMiddleWord(String firstWord) {
--- test/tdd/apr/wordchain/WordChainTest.java
+++ test/tdd/apr/wordchain/WordChainTest.java
@@ -1,16 +1,23 @@
 package tdd.apr.wordchain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WordChainTest {
+    private WordChain wordChain = null;
+
+    @BeforeEach
+    void setUp() {
+        wordChain = new WordChain();
+    }
+
     @Test
     void shouldReturnEmptyString_whenFirstWordAndLastWordAreNotConnected() {
         String firstWord = "egg";
         String lastWord = "jar";
         String dictionary = "egg, jar, cup";
-        WordChain wordChain = new WordChain();
 
         wordChain.setDictionary(dictionary);
 
@@ -24,11 +31,23 @@ class WordChainTest {
         String firstWord = "tap";
         String lastWord = "cup";
         String dictionary = "tap, cap, cup";
-        WordChain wordChain = new WordChain();
 
         wordChain.setDictionary(dictionary);
 
         String expectedWordChain = "tap > cap > cup";
+        String actualWordChain = wordChain.getChain(firstWord, lastWord);
+        assertEquals(expectedWordChain, actualWordChain);
+    }
+
+    @Test
+    void shouldReturnEmptyString_whenLastWordIsNotInDictionary() {
+        String firstWord = "tap";
+        String lastWord = "cup";
+        String dictionary = "tap, cap";
+
+        wordChain.setDictionary(dictionary);
+
+        String expectedWordChain = "";
         String actualWordChain = wordChain.getChain(firstWord, lastWord);
         assertEquals(expectedWordChain, actualWordChain);
     }
