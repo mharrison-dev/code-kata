7dab89239d7fc9ec8bbba8beda8e73f6844deb98
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-04-08 04:21:06 -0600

    Word Chain: should return string with three words when the first word and last word are connected by one word

--- src/tdd/apr/wordchain/WordChain.java
+++ src/tdd/apr/wordchain/WordChain.java
@@ -1,7 +1,46 @@
 package tdd.apr.wordchain;
 
 public class WordChain {
+    private String[] dictionary;
+
     public String getChain(String firstWord, String lastWord) {
-        return "";
+        String middleWord = getMiddleWord(firstWord);
+        if (middleWord == null) {
+            return "";
+        } else {
+            return firstWord + " > " + middleWord + " > " + lastWord;
+        }
+    }
+
+    private String getMiddleWord(String firstWord) {
+        String middleWord = null;
+        for (String word : dictionary) {
+            int hammingDistance = getHammingDistance(firstWord, word);
+            if (hammingDistance == 1) {
+                middleWord = word;
+                break;
+            }
+        }
+
+        return middleWord;
+    }
+
+    private static int getHammingDistance(String word, String otherWord) {
+        int hammingDistance = 0;
+        String[] lettersOfFirstWord = word.split("");
+        String[] lettersOfWord = otherWord.split("");
+        for (int i = 0; i < lettersOfWord.length; i++) {
+            if (!lettersOfWord[i].equals(lettersOfFirstWord[i])) {
+                hammingDistance++;
+            }
+        }
+
+        return hammingDistance;
+    }
+
+    public void setDictionary(String dictionary) {
+        this.dictionary = dictionary
+                .replaceAll("\\s*", "")
+                .split(",");
     }
 }
--- test/tdd/apr/wordchain/WordChainTest.java
+++ test/tdd/apr/wordchain/WordChainTest.java
@@ -12,7 +12,23 @@ class WordChainTest {
         String dictionary = "egg, jar, cup";
         WordChain wordChain = new WordChain();
 
+        wordChain.setDictionary(dictionary);
+
         String expectedWordChain = "";
+        String actualWordChain = wordChain.getChain(firstWord, lastWord);
+        assertEquals(expectedWordChain, actualWordChain);
+    }
+
+    @Test
+    void shouldReturnStringWithThreeWords_whenFirstWordAndLastWordArConnectedByOneWord() {
+        String firstWord = "tap";
+        String lastWord = "cup";
+        String dictionary = "tap, cap, cup";
+        WordChain wordChain = new WordChain();
+
+        wordChain.setDictionary(dictionary);
+
+        String expectedWordChain = "tap > cap > cup";
         String actualWordChain = wordChain.getChain(firstWord, lastWord);
         assertEquals(expectedWordChain, actualWordChain);
     }
