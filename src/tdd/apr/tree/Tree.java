b36fad452f291aff448eee6849afed5a039060bd
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-04-11 03:38:37 -0600

    Tree: should get an empty optional when an empty tree is queried for its children

--- src/tdd/apr/tree/Tree.java
+++ src/tdd/apr/tree/Tree.java
@@ -1,7 +1,13 @@
 package tdd.apr.tree;
 
+import java.util.Optional;
+
 public class Tree {
     public Tree() {
 
+    }
+
+    public Optional<Tree[]> children() {
+        return Optional.empty();
     }
 }
--- test/tdd/apr/tree/TreeTest.java
+++ test/tdd/apr/tree/TreeTest.java
@@ -2,6 +2,8 @@ package tdd.apr.tree;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TreeTest {
@@ -10,5 +12,14 @@ class TreeTest {
         assertDoesNotThrow(() -> {
             Tree tree = new Tree();
         }, "Instantiation of a tree should not throw an exception");
+    }
+
+    @Test
+    void shouldGetEmptyOptional_forChildren_whenTreeIsEmpty() {
+        Tree tree = new Tree();
+
+        Optional<Tree[]> expectedChildren = Optional.empty();
+        Optional<Tree[]> actualChildren = tree.children();
+        assertEquals(expectedChildren, actualChildren);
     }
 }
