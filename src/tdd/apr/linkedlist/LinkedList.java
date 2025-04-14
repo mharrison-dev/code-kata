cdbfe9e3c2d21f264c903680e742c5dcb7a0119c
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-04-14 03:46:54 -0600

    Linked List: should return next node for a node when queried for its next node after its next node has been set

--- src/tdd/apr/linkedlist/LinkedList.java
+++ src/tdd/apr/linkedlist/LinkedList.java
@@ -4,18 +4,25 @@ import java.util.Optional;
 
 public class LinkedList<T> {
     private T value = null;
+    private LinkedList<T> next = null;
 
     public Optional<T> value() {
-        return (value== null)
+        return (value == null)
                 ? Optional.empty()
                 : Optional.of(value);
     }
 
-    public Optional<T> next() {
-        return Optional.empty();
+    public Optional<LinkedList<T>> next() {
+        return (next == null)
+                ? Optional.empty()
+                : Optional.of(next);
     }
 
     public void setValue(T value) {
         this.value = value;
+    }
+
+    public void setNext(LinkedList<T> next) {
+        this.next = next;
     }
 }
--- test/tdd/apr/linkedlist/LinkedListTest.java
+++ test/tdd/apr/linkedlist/LinkedListTest.java
@@ -1,5 +1,6 @@
 package tdd.apr.linkedlist;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
@@ -7,6 +8,13 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LinkedListTest {
+    private LinkedList<String> linkedList = null;
+
+    @BeforeEach
+    void setUp(){
+        linkedList = new LinkedList<>();
+    }
+
     @Test
     void shouldNotThrowException_whenInitializingLinkedList() {
         assertDoesNotThrow(() -> {
@@ -16,8 +24,6 @@ class LinkedListTest {
 
     @Test
     void shouldReturnEmptyOptional_forValue_whenValueHasNotBeenSet() {
-        LinkedList<String> linkedList = new LinkedList<>();
-
         Optional<String> expectedValue = Optional.empty();
         Optional<String> actualValue = linkedList.value();
         assertEquals(expectedValue, actualValue);
@@ -25,21 +31,29 @@ class LinkedListTest {
 
     @Test
     void shouldReturnEmptyOptional_forNext_whenNextNodeHasNotBeenSet() {
-        LinkedList<String> linkedList = new LinkedList<>();
-
-        Optional<String> expectedValue = Optional.empty();
-        Optional<String> actualValue = linkedList.next();
-        assertEquals(expectedValue, actualValue);
+        Optional<LinkedList<String>> expectedNextNode = Optional.empty();
+        Optional<LinkedList<String>> actualNextNode = linkedList.next();
+        assertEquals(expectedNextNode, actualNextNode);
     }
 
     @Test
     void shouldReturnValue_forValue_whenValueHasBeenSet() {
-        LinkedList<String> linkedList = new LinkedList<>();
-
         linkedList.setValue("foo");
 
         Optional<String> expectedValue = Optional.of("foo");
         Optional<String> actualValue = linkedList.value();
         assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    void shouldReturnNextNode_forNext_whenNextNodeHasBeenSet() {
+        LinkedList<String> nextLinkedList = new LinkedList<>();
+
+        linkedList.setNext(nextLinkedList);
+        nextLinkedList.setValue("foo");
+
+        Optional<LinkedList<String>> expectedNextNode = Optional.of(nextLinkedList);
+        Optional<LinkedList<String>> actualNextNode = linkedList.next();
+        assertEquals(expectedNextNode, actualNextNode);
     }
 }
