768f805e55bc2c4a16b71e8a86c83fcc60b32be7
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-04-17 05:07:02 -0600

    Hash Table: should return a unique value for entries that share the same bucket in the hash table

--- src/tdd/apr/hashtable/HashTable.java
+++ src/tdd/apr/hashtable/HashTable.java
@@ -1,25 +1,48 @@
 package tdd.apr.hashtable;
 
+import java.util.LinkedList;
 import java.util.Optional;
 
 public class HashTable<K, V> {
-    private Object[] values = new Object[10];
+    private final LinkedList<KeyValuePair<K, V>>[] buckets = new LinkedList[10];
+
+    public HashTable() {
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
 
     public Optional<V> valueOf(K key) {
-        return (values[indexOf(key)] == null)
-                ? Optional.empty()
-                : Optional.of((V) values[indexOf(key)]);
+        Optional<KeyValuePair<K, V>> keyValuePairOfKey = getKeyValuePairOf(key);
+        return keyValuePairOfKey.map(kvKeyValuePair -> kvKeyValuePair.value);
     }
 
     public void add(K key, V value) {
-        values[indexOf(key)] = value;
+        KeyValuePair<K, V> keyValuePair = new KeyValuePair<>(key, value);
+        LinkedList<KeyValuePair<K, V>> keyValuePairLinkedList = buckets[indexOf(key)];
+        keyValuePairLinkedList.add(keyValuePair);
     }
 
     public void delete(K key) {
-        values[indexOf(key)] = null;
+        Optional<KeyValuePair<K, V>> keyValuePairOfKey = getKeyValuePairOf(key);
+        if (keyValuePairOfKey.isPresent()) {
+            LinkedList<KeyValuePair<K, V>> keyValuePairLinkedList = buckets[indexOf(key)];
+            keyValuePairLinkedList.remove(keyValuePairOfKey.get());
+        }
     }
 
     private int indexOf(K key) {
-        return key.hashCode() % values.length;
+        return key.hashCode() % buckets.length;
+    }
+
+    private Optional<KeyValuePair<K, V>> getKeyValuePairOf(K key) {
+        LinkedList<KeyValuePair<K, V>> keyValuePairLinkedList = buckets[indexOf(key)];
+        return keyValuePairLinkedList.stream()
+                .filter(keyValuePair -> keyValuePair.value != key)
+                .findFirst();
+    }
+
+    private record KeyValuePair<K, V>(K key, V value) {
+
     }
 }
--- test/tdd/apr/hashtable/HashTableTest.java
+++ test/tdd/apr/hashtable/HashTableTest.java
@@ -3,6 +3,7 @@ package tdd.apr.hashtable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Objects;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,7 +12,7 @@ class HashTableTest {
     private HashTable<String, Integer> hashTable = null;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         hashTable = new HashTable<>();
     }
 
@@ -61,6 +62,41 @@ class HashTableTest {
 
         Optional<Integer> expectedValue = Optional.empty();
         Optional<Integer> actualValue = hashTable.valueOf("foo");
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    void shouldReturnUniqueValue_forValueOf_whenHashCollisionsOccur() {
+        class HashCollisionKeyStub {
+            private final String value;
+
+            public HashCollisionKeyStub(String value) {
+                this.value = value;
+            }
+
+            @Override
+            public int hashCode() {
+                return 0;
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                if (o == null || getClass() != o.getClass()) return false;
+                HashCollisionKeyStub that = (HashCollisionKeyStub) o;
+                return Objects.equals(value, that.value);
+            }
+        }
+        HashCollisionKeyStub key = new HashCollisionKeyStub("foo");
+        int value = 600;
+        HashCollisionKeyStub otherKey = new HashCollisionKeyStub("goo");
+        int otherVale = 700;
+        HashTable<HashCollisionKeyStub, Integer> hashTable = new HashTable<>();
+
+        hashTable.add(key, value);
+        hashTable.add(otherKey, otherVale);
+
+        Optional<Integer> expectedValue = Optional.of(600);
+        Optional<Integer> actualValue = hashTable.valueOf(key);
         assertEquals(expectedValue, actualValue);
     }
 }
