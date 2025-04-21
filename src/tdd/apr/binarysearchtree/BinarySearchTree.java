a10410ffd6670acc202122b281c3e80459432093
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-04-21 05:51:02 -0600

    Binary Search Tree: getting all the keys after three insertions returns the keys in pre-order

--- src/tdd/apr/binarysearchtree/BinarySearchTree.java
+++ src/tdd/apr/binarysearchtree/BinarySearchTree.java
@@ -1,27 +1,88 @@
 package tdd.apr.binarysearchtree;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
 public class BinarySearchTree<K, V> {
-    private final List<K> keyList = new ArrayList<>();
-    private V value = null;
+    private Node root = null;
+    private final Comparator<K> keyComparator;
+    
+    public BinarySearchTree(Comparator<K> keyComparator) {
+        this.keyComparator = keyComparator;
+    }
 
-    public Optional<V> getAllValues() {
-        return (value == null)
+    public Optional<List<V>> getAllValues() {
+        return (root == null)
                 ? Optional.empty()
-                : Optional.of(value);
+                : Optional.of(root.getAllValues());
     }
 
     public Optional<List<K>> getAllKeys() {
-        return (keyList.isEmpty())
+        return (root == null)
                 ? Optional.empty()
-                : Optional.of(keyList);
+                : Optional.of(root.getAllKeys());
     }
 
     public void insert(K key, V value) {
-        keyList.add(key);
-        this.value = value;
+        if (root == null) {
+            root = new Node(key, value);
+        } else {
+            root.insert(key, value);
+        }
+    }
+
+    private class Node {
+        private final K key;
+        private final V value;
+        private Node leftChild;
+        private Node rightChild;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public void insert(K key, V value) {
+            switch (keyComparator.compare(key, this.key)) {
+                case -1:
+                    leftChild = new Node(key, value);
+                    break;
+                case 1:
+                    rightChild = new Node(key, value);
+                    break;
+            }
+        }
+
+        public List<V> getAllValues() {
+            List<V> allValues = new ArrayList<>();
+            allValues.add(value);
+
+            if (leftChild != null) {
+                allValues.addAll(leftChild.getAllValues());
+            }
+
+            if (rightChild != null) {
+                allValues.addAll(rightChild.getAllValues());
+            }
+
+            return allValues;
+        }
+
+        public List<K> getAllKeys() {
+            List<K> allKeys = new ArrayList<>();
+            allKeys.add(key);
+
+            if (leftChild != null) {
+                allKeys.addAll(leftChild.getAllKeys());
+            }
+
+            if (rightChild != null) {
+                allKeys.addAll(rightChild.getAllKeys());
+            }
+
+            return allKeys;
+        }
     }
 }
--- test/tdd/apr/binarysearchtree/BinarySearchTreeTest.java
+++ test/tdd/apr/binarysearchtree/BinarySearchTreeTest.java
@@ -2,33 +2,32 @@ package tdd.apr.binarysearchtree;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BinarySearchTreeTest {
     @Test
     void getAllStringValues_emptyTree_returnsEmptyOptional() {
-        BinarySearchTree<String, String> binarySearchTree = new BinarySearchTree<>();
+        BinarySearchTree<String, String> binarySearchTree = new BinarySearchTree<>(getStringComparator());
 
-        Optional<String> expectedValueList = Optional.empty();
-        Optional<String> actualValueList = binarySearchTree.getAllValues();
+        Optional<List<String>> expectedValueList = Optional.empty();
+        Optional<List<String>> actualValueList = binarySearchTree.getAllValues();
         assertEquals(expectedValueList, actualValueList);
     }
 
     @Test
     void getAllIntegerValues_emptyTree_returnsEmptyOptional() {
-        BinarySearchTree<Integer, Integer> binarySearchTree = new BinarySearchTree<>();
+        BinarySearchTree<Integer, Integer> binarySearchTree = new BinarySearchTree<>(getIntegerComparator());
 
-        Optional<Integer> expectedValueList = Optional.empty();
-        Optional<Integer> actualValueList = binarySearchTree.getAllValues();
+        Optional<List<Integer>> expectedValueList = Optional.empty();
+        Optional<List<Integer>> actualValueList = binarySearchTree.getAllValues();
         assertEquals(expectedValueList, actualValueList);
     }
 
     @Test
     void getAllStringKeys_emptyTree_returnsEmptyOptional() {
-        BinarySearchTree<String, String> binarySearchTree = new BinarySearchTree<>();
+        BinarySearchTree<String, String> binarySearchTree = new BinarySearchTree<>(getStringComparator());
 
         Optional<List<String>> expectedKeyList = Optional.empty();
         Optional<List<String>> actualKeyList = binarySearchTree.getAllKeys();
@@ -37,7 +36,7 @@ class BinarySearchTreeTest {
 
     @Test
     void getAllIntegerKeys_emptyTree_returnsEmptyOptional() {
-        BinarySearchTree<Integer, String> binarySearchTree = new BinarySearchTree<>();
+        BinarySearchTree<Integer, String> binarySearchTree = new BinarySearchTree<>(getIntegerComparator());
 
         Optional<List<Integer>> expectedKeyList = Optional.empty();
         Optional<List<Integer>> actualKeyList = binarySearchTree.getAllKeys();
@@ -48,7 +47,7 @@ class BinarySearchTreeTest {
     void getAllKeys_afterOneInsertion_returnsKeyOfInsertedEntry() {
         int key = 0;
         String value = "foo";
-        BinarySearchTree<Integer, String> binarySearchTree = new BinarySearchTree<>();
+        BinarySearchTree<Integer, String> binarySearchTree = new BinarySearchTree<>(getIntegerComparator());
 
         binarySearchTree.insert(key, value);
 
@@ -61,12 +60,12 @@ class BinarySearchTreeTest {
     void getAllValues_afterOneInsertion_returnsValueOfInsertedEntry() {
         int key = 0;
         String value = "foo";
-        BinarySearchTree<Integer, String> binarySearchTree = new BinarySearchTree<>();
+        BinarySearchTree<Integer, String> binarySearchTree = new BinarySearchTree<>(getIntegerComparator());
 
         binarySearchTree.insert(key, value);
 
-        Optional<String> expectedValueList = Optional.of(value);
-        Optional<String> actualValueList = binarySearchTree.getAllValues();
+        Optional<List<String>> expectedValueList = Optional.of(List.of(value));
+        Optional<List<String>> actualValueList = binarySearchTree.getAllValues();
         assertEquals(expectedValueList, actualValueList);
     }
 
@@ -76,7 +75,7 @@ class BinarySearchTreeTest {
         String value = "foo";
         int otherKey = 1;
         String otherValue = "goo";
-        BinarySearchTree<Integer, String> binarySearchTree = new BinarySearchTree<>();
+        BinarySearchTree<Integer, String> binarySearchTree = new BinarySearchTree<>(getIntegerComparator());
 
         binarySearchTree.insert(key, value);
         binarySearchTree.insert(otherKey, otherValue);
@@ -84,5 +83,66 @@ class BinarySearchTreeTest {
         Optional<List<Integer>> expectedKeyList = Optional.of(List.of(key, otherKey));
         Optional<List<Integer>> actualKeyList = binarySearchTree.getAllKeys();
         assertEquals(expectedKeyList, actualKeyList);
+    }
+
+    @Test
+    void getAllValues_afterTwoInsertion_returnsValuesOfInsertedEntriesInPreOrder() {
+        int key = 0;
+        String value = "foo";
+        int otherKey = 1;
+        String otherValue = "goo";
+        BinarySearchTree<Integer, String> binarySearchTree = new BinarySearchTree<>(getIntegerComparator());
+
+        binarySearchTree.insert(key, value);
+        binarySearchTree.insert(otherKey, otherValue);
+
+        Optional<List<String>> expectedKeyList = Optional.of(List.of(value, otherValue));
+        Optional<List<String>> actualKeyList = binarySearchTree.getAllValues();
+        assertEquals(expectedKeyList, actualKeyList);
+    }
+
+    @Test
+    void getAllKeys_afterThreeInsertion_returnsKeysOfInsertedEntriesInPreOrder() {
+        int rootKey = 0;
+        String rootValue = "foo";
+        int leftKey = -1;
+        String leftValue = "boo";
+        int rightKey = 1;
+        String rightValue = "goo";
+        BinarySearchTree<Integer, String> binarySearchTree = new BinarySearchTree<>(getIntegerComparator());
+
+        binarySearchTree.insert(rootKey, rootValue);
+        binarySearchTree.insert(rightKey, rightValue);
+        binarySearchTree.insert(leftKey, leftValue);
+
+        Optional<List<Integer>> expectedKeyList = Optional.of(List.of(rootKey, leftKey, rightKey));
+        Optional<List<Integer>> actualKeyList = binarySearchTree.getAllKeys();
+        assertEquals(expectedKeyList, actualKeyList);
+    }
+
+    private Comparator<Integer> getIntegerComparator() {
+        return new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2);
+            }
+        };
+    }
+
+    private Comparator<String> getStringComparator() {
+        return new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.equals(o2)) {
+                    return 0;
+                }
+
+                String[] strings = {o1, o2};
+                Arrays.sort(strings);
+                return (strings[0].equals(o1))
+                        ? 1
+                        : -1;
+            }
+        };
     }
 }
