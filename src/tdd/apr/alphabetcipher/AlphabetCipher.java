f063c25d56da0c57f92ce8f9c38ff47bc0130361
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-04-22 04:55:40 -0600

    Alphabet Cipher: encoding a single letter returns its corresponding letter

--- src/tdd/apr/alphabetcipher/AlphabetCipher.java
+++ src/tdd/apr/alphabetcipher/AlphabetCipher.java
@@ -1,7 +1,18 @@
 package tdd.apr.alphabetcipher;
 
 public class AlphabetCipher {
-    public String encode(String string) {
-        return "";
+    public String encode(String message, String keyword) {
+        if (message.isEmpty()) {
+            return "";
+        }
+
+        int startingAsciiCodeOfLowercaseLetters = 97;
+        int sizeOfAlphabet = 26;
+
+        int alphabetIndexOfMessageLetter = message.charAt(0) - startingAsciiCodeOfLowercaseLetters;
+        int alphabetIndexOfKeywordLetter = keyword.charAt(0) - startingAsciiCodeOfLowercaseLetters;
+        int alphabetIndexOfEncodedMessageLetter = (alphabetIndexOfMessageLetter + alphabetIndexOfKeywordLetter) % sizeOfAlphabet;
+        char encodedMessageLetter = (char) (alphabetIndexOfEncodedMessageLetter + startingAsciiCodeOfLowercaseLetters);
+        return String.valueOf(encodedMessageLetter);
     }
 }
--- test/tdd/apr/alphabetcipher/AlphabetCipherTest.java
+++ test/tdd/apr/alphabetcipher/AlphabetCipherTest.java
@@ -1,16 +1,34 @@
 package tdd.apr.alphabetcipher;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AlphabetCipherTest {
     @Test
     void encode_emptyString_returnsEmptyString() {
+        String message = "";
+        String keyword = "a";
         AlphabetCipher alphabetCipher = new AlphabetCipher();
 
         String expectedString = "";
-        String actualString = alphabetCipher.encode("");
+        String actualString = alphabetCipher.encode(message, keyword);
+        assertEquals(expectedString, actualString);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a,b", "l,m", "x,y", "z,a"})
+    void encode_singleLetter_returnsCorrespondingLetter(String messageAndEncodedMessage) {
+        String[] messageAndEncodedMessageArray = messageAndEncodedMessage.split(",");
+        String message = messageAndEncodedMessageArray[0];
+        String encodedMessage = messageAndEncodedMessageArray[1];
+        String keyword = "b";
+        AlphabetCipher alphabetCipher = new AlphabetCipher();
+
+        String expectedString = encodedMessage;
+        String actualString = alphabetCipher.encode(message, keyword);
         assertEquals(expectedString, actualString);
     }
 }
