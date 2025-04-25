aa0f50bbc47d4de3d2f671b7b5b6f1c6233f22a7
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-04-25 04:08:54 -0600

    Integer Heap 2nd Try: insert an integer into an empty heap

--- src/tdd/apr/integerheapsecondtry/IntegerHeap.java
+++ src/tdd/apr/integerheapsecondtry/IntegerHeap.java
@@ -3,7 +3,23 @@ package tdd.apr.integerheapsecondtry;
 import java.util.Optional;
 
 public class IntegerHeap {
+    private Node root;
+
     public Optional<Integer> extract() {
-        return Optional.empty();
+        return (root == null)
+                ? Optional.empty()
+                : Optional.of(root.integer);
+    }
+
+    public void insert(int integer) {
+        root = new Node(integer);
+    }
+
+    private static class Node {
+        private final int integer;
+
+        public Node(int integer) {
+            this.integer = integer;
+        }
     }
 }
--- test/tdd/apr/integerheapsecondtry/IntegerHeapTest.java
+++ test/tdd/apr/integerheapsecondtry/IntegerHeapTest.java
@@ -15,4 +15,16 @@ class IntegerHeapTest {
         Optional<Integer> actualInteger = integerHeap.extract();
         assertEquals(expectedInteger, actualInteger);
     }
+
+    @Test
+    void insert_emptyHeap() {
+        int firstInteger = 1;
+        IntegerHeap integerHeap = new IntegerHeap();
+
+        integerHeap.insert(firstInteger);
+
+        Optional<Integer> expectedInteger = Optional.of(firstInteger);
+        Optional<Integer> actualInteger = integerHeap.extract();
+        assertEquals(expectedInteger, actualInteger);
+    }
 }
